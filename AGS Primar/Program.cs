3fd4c96e5e875aa99f4146a24fb5290e77bff3d9
Bojan Skopljak <skopljak.pr117.2020@uns.ac.rs>
2023-10-21 14:19:09 +0200

    pocetni kod proba

---
+++ AGS Primar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using System.ServiceModel;
+
+namespace AGS_Primar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Servis1)))
+            {
+                host.Open();
+                Console.WriteLine("Servis je uspesno pokrenut");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Common;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<Interface1> channel =
+                new ChannelFactory<Interface1>("ServiceName");
+
+            Interface1 proxy = channel.CreateChannel();
+        }
+    }
+}
