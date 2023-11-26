fb367bade559c3edb6cd94c96984130f3c677986
12nikola <102314632+12nikola@users.noreply.github.com>
2023-11-26 20:53:15 +0100

    sertifikati

--- Common/IAGSPrimar.cs
+++ Common/IAGSPrimar.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
-{
+{   
+    [ServiceContract]
     public interface IAGSPrimar
     {
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        void AddAlarm();
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        void DeleteAlarm();
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        void AcceptDelete();
+
     }
 }
